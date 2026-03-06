bc4142b956e6b75ff581265db5b0521aa64ce67c
Haidhi Angkawijana <haidhiangkawijana@gmail.com>
2026-03-06 09:32:33 +0700

    kitty

---
+++ .config/kitty/tab_bar.py
@@ -0,0 +1,43 @@
+import datetime
+import json
+import subprocess
+from collections import defaultdict
+
+from kitty.boss import get_boss
+from kitty.fast_data_types import Screen, add_timer
+from kitty.tab_bar import (
+    DrawData,
+    ExtraData,
+    Formatter,
+    TabBarData,
+    as_rgb,
+    draw_attributed_string,
+    draw_tab_with_powerline,
+)
+
+timer_id = None
+
+
+def draw_tab(
+    draw_data: DrawData,
+    screen: Screen,
+    tab: TabBarData,
+    before: int,
+    max_title_length: int,
+    index: int,
+    is_last: bool,
+    extra_data: ExtraData,
+) -> int:
+    global timer_id
+
+    # if timer_id is None:
+    #     timer_id = add_timer(_redraw_tab_bar, 2.0, True)
+    draw_tab_with_powerline(
+        draw_data, screen, tab, before, max_title_length, index, is_last, extra_data
+    )
+    return screen.cursor.x
+
+
+
+
+
